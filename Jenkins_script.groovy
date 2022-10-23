f4e8fb21d6552d2edc7a6279d1521ac43ebdd237
NAFLUDA <115708225+NAFLUDA@users.noreply.github.com>
2022-10-23 22:42:17 +0300

    Update Jenkins_script.groovy

--- Jenkins_script.groovy
+++ Jenkins_script.groovy
@@ -1,5 +1,10 @@
 pipeline {
     agent any
+    environment {
+        dokerImage = ''
+        registry = 'nafluda/pythonapp'
+        registryCredencial = 'aa93560d-b255-423e-a012-b8d31c9ab8af'
+    }
 
     stages {
         stage('Checkout') {
@@ -8,16 +13,27 @@ pipeline {
                checkout([$class: 'GitSCM', branches: [[name: '*/Python']], extensions: [], userRemoteConfigs: [[credentialsId: 'f9e6b84d-2d59-44ff-9ccf-b1d16bc98954', url: 'https://github.com/NAFLUDA/Test_repository.git']]])
             }
         }
-        stage('Build') {
+        stage('Test') {
             steps {
-                echo 'Luda Build'
+                echo 'Luda Test'
                 git branch: 'python', credentialsId: 'f9e6b84d-2d59-44ff-9ccf-b1d16bc98954', url: 'https://github.com/NAFLUDA/Test_repository.git'
                 bat 'python L_python.py'
             }
         }
-        stage('Test') {
+        stage('Build Docker image') {
             steps {
-            echo 'Luda test'
+                 script {
+                    dockerImage = docker.build registry
+                 }
+            }
+        }
+        stage('Uploading Docker image') {
+            steps {
+                 script {
+                    docker.withRegistry ( '', registryCredencial ) {
+                    dockerImage.push()
+                    }
+                 }
             }
         }
         
